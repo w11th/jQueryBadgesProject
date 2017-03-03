b565cd59f6e95f4e3b94f3142f4931fde8e60ed0
Wenhui Wang <w.eleventh@gmail.com>
2017-03-03 12:29:30 +0800

    update data fetching

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,28 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax('https://www.codeschool.com/users/w11th.json', { dataType: 'jsonp' }).done(function(result) {
+    var courses = result.courses.completed;
+    let coursesDivs = courses.map(function(course) {
+      let courseDiv = $('<div />', {
+        'class': 'course'
+      });
+      $('<h3 />', {
+        text: course.title
+      }).appendTo(courseDiv);
+      $('<img />', {
+        src: course.badge,
+        alt: course.title + " badge image"
+      }).appendTo(courseDiv);
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        'class': 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo(courseDiv);
+      return courseDiv;
+    });
+    $('#badges').append(coursesDivs);
+  }).fail(function(jqxhr, textStatus, error) {
+    var err = textStatus + ", " + error;
+    console.log(err);
+  });
 });
