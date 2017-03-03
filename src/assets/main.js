908d775784ad8e515c374e5a6999b005445813e2
Wenhui Wang <w.eleventh@gmail.com>
2017-03-03 12:40:52 +0800

    update ajax to callback style

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,28 +1,32 @@
 $(function() {
-  $.ajax('https://www.codeschool.com/users/w11th.json', { dataType: 'jsonp' }).done(function(result) {
-    var courses = result.courses.completed;
-    let coursesDivs = courses.map(function(course) {
-      let courseDiv = $('<div />', {
-        'class': 'course'
+  $.ajax('https://www.codeschool.com/users/w11th.json', {
+    dataType: 'jsonp',
+    success: function(result) {
+      var courses = result.courses.completed;
+      let coursesDivs = courses.map(function(course) {
+        let courseDiv = $('<div />', {
+          'class': 'course'
+        });
+        $('<h3 />', {
+          text: course.title
+        }).appendTo(courseDiv);
+        $('<img />', {
+          src: course.badge,
+          alt: course.title + " badge image"
+        }).appendTo(courseDiv);
+        $('<a />', {
+          href: course.url,
+          target: '_blank',
+          'class': 'btn btn-primary',
+          text: 'See Course'
+        }).appendTo(courseDiv);
+        return courseDiv;
       });
-      $('<h3 />', {
-        text: course.title
-      }).appendTo(courseDiv);
-      $('<img />', {
-        src: course.badge,
-        alt: course.title + " badge image"
-      }).appendTo(courseDiv);
-      $('<a />', {
-        href: course.url,
-        target: '_blank',
-        'class': 'btn btn-primary',
-        text: 'See Course'
-      }).appendTo(courseDiv);
-      return courseDiv;
-    });
-    $('#badges').append(coursesDivs);
-  }).fail(function(jqxhr, textStatus, error) {
-    var err = textStatus + ", " + error;
-    console.log(err);
+      $('#badges').append(coursesDivs);
+    },
+    error: function(jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error;
+      console.log(err);
+    }
   });
 });
